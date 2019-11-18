ed611c0eb87556033d2f86a5ef2dd1e3809baccb
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2019-11-18 05:21:22 +0000

    Made some animation frames...
    ... but they don't seem to be working with the sprite. :/

---
+++ Assets/Scripts/PlayerItself.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerItself : MonoBehaviour
+{
+    private Animator swipeAnim;
+    private void Start()
+    {
+        swipeAnim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            swipeAnim.SetBool("Firing?", true);
+        }
+        else
+        {
+            swipeAnim.SetBool("Firing?", false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerItselfII.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerItselfII : MonoBehaviour
+{
+    private Animator walkAnim;
+    private void Start()
+    {
+        walkAnim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            walkAnim.SetBool("Moving?", true);
+        }
+        else
+        {
+            walkAnim.SetBool("Moving?", false);
+        }
+    }
+}
