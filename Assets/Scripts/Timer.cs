0383e2ee6a8ab8bbe51a52dda6e356c111cd92b7
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2019-12-09 16:10:08 +0000

    Adding Random Spawn Timer
    What have we learnt today? InvokeRepeat is a nasty piece of work.
    
    - Removed InvokeRepeat and opted for a deltaTime component to make the random spawn timer. Less argument clashes.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 using UnityEngine.Events;
 public class Timer : MonoBehaviour
 {
-    public float time = 1;
+    public float Min = 1;
+    public float Max = 10;
+    private float timer = 0;
+    private int waitTime;
     public bool repeat = false;
+
     public UnityEvent onTimerComplete;
+
     private void Start()
+    {
+        System.Random NumberGenerator = new System.Random();
+        waitTime = NumberGenerator.Next((int)Min, (int)Max);
+
+ 
+    }
+ 
+    private void Update()
     {
         if (repeat)
         {
-            InvokeRepeating("OnTimerComplete", 0, time);
+
+            //InvokeRepeating("OnTimerComplete", 0, NumberGenerator.Next((int)Min, (int)Max));
+            if (timer > waitTime)
+            {
+                
+                System.Random NumberGenerator = new System.Random();
+                OnTimerComplete();
+                timer = 0;
+                waitTime = NumberGenerator.Next((int)Min, (int)Max);
+            }
         }
         else
         {
-            Invoke("OnTimerComplete", time);
+            System.Random NumberGenerator = new System.Random();
+            Invoke("OnTimerComplete", NumberGenerator.Next((int)Min, (int)Max));
         }
+        timer += Time.deltaTime;
+
+
     }
+
     private void OnTimerComplete()
     {
+        Debug.Log("test");
         onTimerComplete.Invoke();
+
     }
+
 }
